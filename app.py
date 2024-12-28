2e639a13688ba5e69bb79e9cc84d05e8171cd429
Andrei Turgo <abturgo@gmail.com>
2024-12-28 22:40:02 +0800

    UserRegister partial creation

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
+from models.pet_owners import Pet_owners
 
 app = Flask(__name__)
 
 @app.route("/")
+def petOwnerRegisterLink():
+    
+    name = request.args.get("name", "world")
+    
+    return render_template("user_registration.html", name = name)
 
-def petOwnerRegister():
-    return "Register"
+#User Registration
+@app.route("/userRegistration", methods=["GET", "POST"])
+def petOwnerRegisterReceived():
+    form_data = request.form
+    Pet_owners.form_register(form_data)
+    
+    
+    
+    
--- models/pet_owners.py
+++ models/pet_owners.py
@@ -5,4 +5,14 @@ class Pet_owners(Person):
         super().__init__(first_name, last_name, email, username, password)
         self.user_id = user_id
         self.address_id = address_id
+    
+    def form_register(cls, form_data): #Form retirieval
+        
+        first_name = form_data.get("first_name")
+        last_name = form_data.get("last_name")
+        email = form_data.get("email")
+        username = form_data.get("username")
+        password = form_data.get("password")
+        
+        
         
